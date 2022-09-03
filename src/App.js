c949c74a3a29eb3d6de389419d5c2360f93badd2
ihateyou <natepsacc@gmail.com>
2022-09-03 16:43:35 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const express=require('express');
+const app=express();
 
+const { blogEasy, upload, getPost, getBlog, deletePost, getRecent } = require('blog-easy');
+
+blogEasy({
+    static : 'public', // replace with your own public directory
+    app : app,
+    database : 'your mongoDB string url'
+});
 function App() {
   return (
     <div className="App">
